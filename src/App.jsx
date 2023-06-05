ea03abebc5aaeacd9736593737f4f35d82fec66f
Mojaer Ahmed <mojaermohi@gmail.com>
2023-06-05 11:24:45 +0600

    header is implemented

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import Header from './Shared/Header/Header'
+import Footer from './Shared/Footer/Footer'
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
